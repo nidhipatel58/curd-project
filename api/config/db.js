ccafe11f1d2665c1ca3b6c46583f430e8bef9eb1
nidhipatel <5808nidhipatel>
2025-01-27 15:54:39 +0530

    changes

--- api/config/db.js
+++ api/config/db.js
@@ -8,34 +8,34 @@ const configModule = {
     dialect: "postgres",
     logging: false,
   },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.TEST_DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    logging: false,
-  },
-  production: {
-    use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
-    logging: false,
-  },
+  // test: {
+  //   username: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  //   database: process.env.TEST_DB_NAME,
+  //   host: process.env.DB_HOST,
+  //   dialect: "postgres",
+  //   logging: false,
+  // },
+  // production: {
+  //   use_env_variable: "DATABASE_URL",
+  //   dialect: "postgres",
+  //   logging: false,
+  // },
 };
 
 const env = process.env.NODE_ENV || "development";
 const config = configModule[env];
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+// }
 
 export default sequelize;
